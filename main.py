09dc6741f66a87a8e0de300dc7d78ebf49a267fe
jla690 <jla690@sfu.ca>
2025-09-10 14:15:28 -0700

    update to remove my own data

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 import json
-import os
+from types import SimpleNamespace
 
 url = 'https://graphql.anilist.co'
 file_path = 'data.json'
-query = '''
+list_from_user_query = '''
 query ($type: MediaType!, $userId: Int!) {
   MediaListCollection(type: $type, userId: $userId) {
     lists {
@@ -33,28 +33,32 @@ variables = {
 
 def api_call():
     try:
-        return requests.post(url, json={'query': query, 'variables': variables})
+        return requests.post(url, json={'query': list_from_user_query, 'variables': variables})
     except:
         print("Error with POST request")
 
 def file_writing(obj):
     if obj is None:
-        exit(1)
-    if not os.path.exists('data.json'):
-        with open('data.json', 'w') as f:
-            json.dump(obj, f, ensure_ascii=False, indent=2)
+        print("JSON object is empty")
+        return
+    with open('data.json', 'w') as f:
+        json.dump(obj, f, ensure_ascii=False, indent=2)
 
 
 def main():
-    json_object = None
+    print("Type in the userId")
+    
+    json_object_for_print = None
     response = api_call()
     try:
-        json_object = json.loads(response.text)
+        json_object_for_print = json.loads(response.text)
     except:
         print("Error parsing text")
         exit(1)
 
-    file_writing(json_object)
+    file_writing(json_object_for_print)
+    json_object = json.loads(response.text, object_hook=SimpleNamespace)
+    print(json_object)
     
 
 if __name__ == '__main__':
