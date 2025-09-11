db81e34f0007fc3527f3011468961a3c724f6f76
jla690 <jla690@sfu.ca>
2025-09-10 23:04:51 -0700

    add some more

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 import random
 import time
-import requests
 import json
 import webbrowser
 import os
-from urllib.parse import quote
+import requests
 
-login_url = "https://anilist.co/api/v2/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
-graphql_url = 'https://graphql.anilist.co'
-token_url = "https://anilist.co/api/v2/oauth/token"
-file_path = 'data.json'
-client_id = int(os.getenv("ANILIST_CLIENT_ID", "30162"))
+LOGIN_URL = "https://anilist.co/api/v2/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
+GRAPHQL_URL = "https://graphql.anilist.co"
+TOKEN_URL = "https://anilist.co/api/v2/oauth/token"
+FILE_PATH = "data.json"
+client_id = int(os.getenv("ANILIST_CLIENT_ID", ""))
 client_secret = os.getenv("ANILIST_CLIENT_SECRET", "")
-redirect_uri = "https://anilist.co/api/v2/oauth/pin"
-token_path = 'token.json'
-list_from_user_query = '''
+REDIRECT_URL = "https://anilist.co/api/v2/oauth/pin"
+TOKEN_PATH = "token.json"
+
+RECOMMENDATIONS_QUERY = """
+query ($search: String!) {
+  Media(search: $search) {
+    id
+    title {
+      romaji 
+    }
+    recommendations(perPage: 20, sort: RATING_DESC) {
+      edges {
+        node {
+          mediaRecommendation {
+            id
+            averageScore
+            popularity
+            title {
+              romaji native english
+                }
+            genres
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+LIST_FROM_USER_QUERY = """
 query ($type: MediaType!, $userId: Int!) {
   MediaListCollection(type: $type, userId: $userId) {
     lists {
@@ -34,61 +60,77 @@ query ($type: MediaType!, $userId: Int!) {
     }
   }
 }
-'''
+"""
 
-variables = {
-    'id': 15125,
-    'type': "MANGA",
-    'userId': 7483344
-}
+variables = {"type": "MANGA", "userId": 7483344}
+
 
 def build_login_url():
-    return login_url.format(client_id=client_id, redirect_uri=redirect_uri)
+    return LOGIN_URL.format(client_id=client_id, redirect_uri=REDIRECT_URL)
 
-def api_call():
+
+def api_call(query):
     try:
         token = authorization()
         headers = {
-            'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json',
-			'Accept': 'application/json'
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+            "Accept": "application/json",
         }
-        return requests.post(graphql_url, json={'query': list_from_user_query, 'variables': variables}, headers=headers)
-    except:
-        print("Error with POST request")
+        response = requests.post(
+            GRAPHQL_URL,
+            json={"query": query, "variables": variables},
+            headers=headers,
+            timeout=20,
+        )
+        return response
+    except requests.RequestException as e:
+        print(f"Error with POST request: {e}")
+
 
 def token_conversion(code):
-    response = requests.post(token_url, json={
-        'grant_type': 'authorization_code',
-        'client_id': client_id, 
-        'client_secret': client_secret, 
-        'redirect_uri': redirect_uri, 
-        'code': code})
+    response = requests.post(
+        TOKEN_URL,
+        json={
+            "grant_type": "authorization_code",
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "redirect_uri": REDIRECT_URL,
+            "code": code,
+        },
+        timeout=20
+    )
     json_object = response.json()
     return json_object
+
+
+def add_map(key, value):
+    variables[key] = value
 
 def file_writing(obj):
     if obj is None:
         print("JSON object is empty")
         return
-    with open('data.json', 'w') as f:
+    with open("data.json", "w") as f:
         json.dump(obj, f, ensure_ascii=False, indent=2)
 
+
 def get_token_from_file():
-    if not os.path.exists(token_path):
+    if not os.path.exists(TOKEN_PATH):
         return None
-    with open(token_path) as f:
+    with open(TOKEN_PATH) as f:
         token = json.load(f)
         if time.time() >= token["expiration_time"]:
             return None
         return token
+
 
 def save_token(token):
     if token is None:
         print("Can't save None token")
         return
     token["expiration_time"] = time.time() + token["expires_in"]
-    with open(token_path, 'w') as f:
+    with open(TOKEN_PATH, "w") as f:
         json.dump(token, f, indent=2)
 
 def authorization():
@@ -103,26 +145,45 @@ def authorization():
     save_token(json_object)
     return json_object["access_token"]
 
+
 def get_random_manga(json_object):
     all_titles = []
     for arr in json_object["data"]["MediaListCollection"]["lists"]:
         for entry in arr["entries"]:
             all_titles.append(entry["media"])
-    
+
     random_choice = random.choice(all_titles)
+    print("\n-----------------------")
     print("Random Pick: ", json.dumps(random_choice, ensure_ascii=False, indent=2))
 
+
+def get_recommendations(json_object):
+    for edge in json_object["data"]["Media"]["recommendations"]["edges"]:
+        print(
+            json.dumps(
+                edge["node"]["mediaRecommendation"], ensure_ascii=False, indent=2
+            )
+        )
+
+
 def main():
+    print("Which query to run?")
+    choice = input("1. Recommendation\n2. Random manga from your list\n")
+    response = None
     json_object = None
-    response = api_call()
-    try:
+    if choice == "1":
+        search = input("Title of Manga: ")
+        add_map("search", search)
+        response = api_call(RECOMMENDATIONS_QUERY)
         json_object = response.json()
-    except:
-        print("Error parsing text")
-        exit(1)
+        get_recommendations(json_object)
+    elif choice == "2":
+        response = api_call(LIST_FROM_USER_QUERY)
+        json_object = response.json()
+        get_random_manga(json_object)
 
     file_writing(json_object)
-    get_random_manga(json_object)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
